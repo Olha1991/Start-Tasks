1e3018285fc60047c0899044a7e7a666edfd581e
Homework1 <danisholha760@gmail.com>
2025-02-06 16:34:42 -0600

    added task conditions

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,58 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Start!");
+        //Task 1
+        //Using a conditional operator, write a program that prints to the console:
+        //a message that the person is 18 or older;
+        //a message that the person is not yet of legal age, you need to wait a little.
+        //When each condition is met, the program should print to the console a message in the format: "If the person is ...,
+        // then (output depending on the result) he is an adult / he is not yet of legal age, you need to wait a little."
+
+
+        //Task 2
+        //A person dresses depending on the air temperature. Write a program that displays the message:
+        //"It's cold outside, you need to put on a hat" - if the temperature is below 5 degrees.
+        //"It's warm today, you can go without a hat" - if the air temperature is above 5 degrees.
+        //When each condition is met, display on the console: "It's ... degrees outside, (output depending on the result)
+        // you need to put on a hat/you can go without a hat".
+
+        //Task3
+        //A driver may be issued a speeding ticket. Write a program that informs the driver that:
+        //the speed is exceeded if it is more than 60 km/h;
+        //the speed is not exceeded if it is less than 60 km/h.
+        //When each condition is met, the program should output the following message: "If the speed is ...,
+        // then (output depending on the result) you will have to pay a fine / you can drive safely."
+
+        //Task 4
+        //Write a program that will help determine which institution a person should be sent to depending on their age:
+        //If a person is between 2 and 6 years old, then they need to go to kindergarten.
+        //If a person is between 7 and 17 years old, then they need to go to school.
+        //If a person is greater than or equal to 18 but less than or equal to 24, then their place is at university.
+        //And if a person is greater than 24, then it's time for them to go to work.
+        //When each condition is met, the program should output a message to the console in the following format:
+        // "If a person's age is ..., then they need to go to ... (add the necessary values depending on age)."
+
+        //Task 5
+        //As a rule, there are age restrictions on rides for children:
+        //If a child is under 5 years old, he cannot ride the ride.
+        //If a child is over 5 years old but under 14 years old, he can ride only if accompanied by an adult. If there
+        // is no adult, he cannot ride.
+        //If a child is over 14 years old, he can ride without an adult.
+        //Write a program that prints a message to the console in the following format: "If the child is ...,
+        // then he ... (depending on the age, add what is necessary: cannot ride the ride, can with / without an adult)".
+
+        //Task 6
+        //The capacity of one train car is 102 people. The car is designed for 60 seats, all the rest are standing.
+        //Using the conditional operator and the else construct, write a program that prints to the console a message
+        // about whether the car has a seat, a seat, a standing place, or whether the car is already completely full.
+
+        //Task 7
+        //Given three numbers:
+        //int one;
+        //int two;
+        //int three
+        //Using a conditional statement and the else construct, write a program that calculates which of the three
+        // numbers is greater and prints the result to the console.
+
     }
 }
